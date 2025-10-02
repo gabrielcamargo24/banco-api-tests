6f4f8af4990e252bed8947e6d3cae1f952f46af8
Camargo <camargogabriel.v@gmail.com>
2025-10-02 05:33:04 -0300

    adc o beforeEach para capturar o nosso códigotoken

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -5,10 +5,15 @@ const {obterToken} = require ('../helpers/autenticacao.js');
 
 describe('Transferencias', () => {
    describe('POST /transferencias', () => {
+    let token   
+    
+    beforeEach(async () => {
+             token = await obterToken('julio.lima', '123456');
+        })
+
       it('should return 201 OK para tranferencias igual ou maior que 10,00', async () => {
         
-        const token = await obterToken('julio.lima', '123456');
-        
+      
         const response = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
@@ -24,7 +29,6 @@ describe('Transferencias', () => {
               });
 
      it('should return 422 quando for transferencias menores que 10,00', async () => {
-        const token = await obterToken('julio.lima', '123456');
         
         const response = await request(process.env.BASE_URL)
             .post('/transferencias')
