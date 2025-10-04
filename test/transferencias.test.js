85d60686202bca141595eaec7cef2e49ca9ab1f7
Camargo <camargogabriel.v@gmail.com>
2025-10-04 16:12:27 -0300

    adicionei testes para GET transferencias total e individual

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -5,14 +5,12 @@ const {obterToken} = require ('../helpers/autenticacao.js');
 const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferencias', () => {
-   describe('POST /transferencias', () => {
-    let token;    
-    
+        let token;    
     beforeEach(async () => {
              token = await obterToken('julio.lima', '123456');
         })
-
-      it('should return 201 OK para tranferencias igual ou maior que 10,00', async () => {
+   describe('POST /transferencias', () => {
+          it('should return 201 OK para tranferencias igual ou maior que 10,00', async () => {
         const bodyTransferencia = {...postTransferencias}
         const response = await request(process.env.BASE_URL)
             .post('/transferencias')
@@ -20,7 +18,6 @@ describe('Transferencias', () => {
             .set('Authorization', `Bearer ${token}`)
             .send(bodyTransferencia);
             expect(response.status).to.equal(201);
-            console.log(response.body);
               });
 
      it('should return 422 quando for transferencias menores que 10,00', async () => {
@@ -35,4 +32,31 @@ describe('Transferencias', () => {
               });
 
            });
-        });
+    describe('GET /transferencias/{id}', () => {
+        it ('should return 200 OK e listar os dados igual ao registro de transferencia igual ao contido no banco de dados quando o registro for válido', async () => {
+            const response = await request(process.env.BASE_URL)
+            .get('/transferencias/4')
+            .set('Authorization', `Bearer ${token}`)
+
+            expect(response.status).to.equal(200)
+            expect(response.body.id).to.equal(4)
+            expect(response.body.id).to.be.a('number')
+            expect(response.body.conta_origem_id).to.equal(4)
+            expect(response.body.conta_destino_id).to.equal(5)
+            expect(response.body.valor).to.equal(11.00) //no swagger deveria ser um float mas está como string
+            expect(response.body.valor).to.be.a('number'); 
+            
+        })
+    })
+    describe ('GET /transferencias', () => {
+        it ('Deve retornar 10 elementos na paginação quando informar o limite de 10 registros', async () => {
+            const response = await request(process.env.BASE_URL)
+            .get('/transferencias?page=1&limit=10')
+            .set('Authorization', `Bearer ${token}`);
+            
+            expect(response.status).to.equal(200);
+            expect (response.body.limit).to.equal(10);
+            expect (response.body.transferencias).to.have.lengthOf(10);
+        })
+    })
+ })        
