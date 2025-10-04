ff3ed70738f003d4b775266b6eacb23db242e08f
Camargo <camargogabriel.v@gmail.com>
2025-10-04 11:04:02 -0300

    adcionando fixtures para gerir os corpos

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -2,44 +2,35 @@ const request = require('supertest');
 const {expect} = require ('chai');
 require('dotenv').config();
 const {obterToken} = require ('../helpers/autenticacao.js');
+const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferencias', () => {
    describe('POST /transferencias', () => {
-    let token   
+    let token;    
     
     beforeEach(async () => {
              token = await obterToken('julio.lima', '123456');
         })
 
       it('should return 201 OK para tranferencias igual ou maior que 10,00', async () => {
-        
-      
+        const bodyTransferencia = {...postTransferencias}
         const response = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 4,
-                contaDestino: 5,
-                valor: 11,
-                token:""
-                    });
+            .send(bodyTransferencia);
             expect(response.status).to.equal(201);
             console.log(response.body);
               });
 
      it('should return 422 quando for transferencias menores que 10,00', async () => {
-        
+        const bodyTransferencia = {...postTransferencias}
+        bodyTransferencia.valor = 7;
         const response = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 4,
-                contaDestino: 5,
-                valor: 5,
-                token:""
-                    });
+            .send(bodyTransferencia);
             expect(response.status).to.equal(422);
               });
 
